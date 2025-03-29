729ecb4489fb68cea3a6d7de5495c7608e27cf7c
Dev pratap singh <141826666+D-P-Singh@users.noreply.github.com>
2025-03-29 22:11:30 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express")
 const { createUsers, loginUsers, updatePassword,sendOtp, otpVerification } = require("../controllers/user_controller")
 const { createTodo } = require("../controllers/todo_controller")
 const auth = require("../middleware/authrization")
+const { userInfo } = require("../controllers/userInfo")
 const router = express.Router()
 
 router.post("/signup", createUsers);
@@ -10,5 +11,6 @@ router.post("/login", loginUsers);
 router.put("/updatepasssword", auth.auth, updatePassword);
 router.post("/sendotp",sendOtp)
 router.post("/otpverification", otpVerification)
+router.get("/userinfo", auth.auth, userInfo)
 module.exports = router;
 
