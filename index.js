7b6fe392f14d1bea78209d1d4f547b307c1c4f6f
Dev pratap singh <141826666+D-P-Singh@users.noreply.github.com>
2025-03-31 22:17:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors")
 require("dotenv").config()
 const app = express()
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://todotaska.netlify.app",
     methods: ["POST", "PUT", "DELETE", "GET"],
   //  headers: ['Content-Type', 'Authrization']
 }))
